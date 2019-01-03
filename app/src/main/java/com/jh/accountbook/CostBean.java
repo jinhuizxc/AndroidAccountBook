d85a2de09d84ac7d972056a0923e7a41f9b7ccf4
jinhuizxc <1004260403@qq.com>
2019-01-03 12:06:22 +0800

    Initial commit

---
+++ app/src/main/java/com/jh/accountbook/CostBean.java
@@ -0,0 +1,49 @@
+package com.jh.accountbook;
+
+import java.io.Serializable;
+
+/**
+ * Created by  Mr.Robot on 2017/3/26.
+ * zhouchatain@gmail.com
+ * GitHub:https://github.com/TheSadFrog
+ */
+
+public class CostBean implements Serializable {
+
+    public String costTitle;
+    public String costDate;
+    public String costMoney;
+
+    public String getCostTitle() {
+        return costTitle;
+    }
+
+    public void setCostTitle(String costTitle) {
+        this.costTitle = costTitle;
+    }
+
+    public String getCostDate() {
+        return costDate;
+    }
+
+    public void setCostDate(String costDate) {
+        this.costDate = costDate;
+    }
+
+    public String getCostMoney() {
+        return costMoney;
+    }
+
+    public void setCostMoney(String costMoney) {
+        this.costMoney = costMoney;
+    }
+
+    @Override
+    public String toString() {
+        return "CostBean{" +
+                "costTitle='" + costTitle + '\'' +
+                ", costDate='" + costDate + '\'' +
+                ", costMoney='" + costMoney + '\'' +
+                '}';
+    }
+}
